f5e2a752728c8b235a858233078359017c538f5a
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-10 21:19:46 +0400

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,33 @@ videos.forEach(video => {
 
 video.addEventListener("click", () => {
 
+videos.forEach(v=>{
+v.muted = true;
+});
+
 video.muted = false;
 video.play();
+
+});
+
+});
+
+window.addEventListener("scroll", ()=>{
+
+videos.forEach(video=>{
+
+const rect = video.getBoundingClientRect();
+
+if(rect.top >= 0 && rect.bottom <= window.innerHeight){
+
+video.play();
+
+}else{
+
+video.pause();
+video.muted = true;
+
+}
 
 });
 
