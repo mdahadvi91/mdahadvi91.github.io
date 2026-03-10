9b3eb991f79e5fb9bcbb964e56cb0298d5fedff0
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-10 13:15:02 +0400

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,5 +1,8 @@
+const feed = document.getElementById("video-feed");
+
 const video = document.createElement("video");
 video.src = "https://www.w3schools.com/html/mov_bbb.mp4";
 video.controls = true;
 video.autoplay = true;
+
 feed.appendChild(video);
