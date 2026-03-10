44583968246e93aa77191e6bc9ce3ffeffc84f6b
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-10 21:27:36 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,15 @@
-const videos = document.querySelectorAll("#video-feed video");
+const videos = document.querySelectorAll("video");
 
-// tap করলে sound on
 videos.forEach(video => {
 
-video.addEventListener("click", () => {
+video.addEventListener("click", function(){
 
-videos.forEach(v=>{
-v.muted = true;
-});
+if(video.muted){
 
 video.muted = false;
-video.play();
-
-});
-
-});
-
-
-// scroll করলে শুধু screen এর video play হবে
-window.addEventListener("scroll", () => {
-
-videos.forEach(video => {
-
-const rect = video.getBoundingClientRect();
-
-if(rect.top >= 0 && rect.bottom <= window.innerHeight){
-
-video.play();
 
 }else{
 
-video.pause();
 video.muted = true;
 
 }
