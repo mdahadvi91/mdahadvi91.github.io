3d314f6a826dfb6d17d55617778591e83d6a6ee0
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-10 09:17:34 +0400

    Update profile.js

--- profile.js
+++ profile.js
@@ -2,20 +2,33 @@ import { auth, db } from "./firebase.js";
 import { collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const profileVideos = document.getElementById("profileVideos");
+const username = document.getElementById("username");
 
 auth.onAuthStateChanged(async user=>{
-  if(!user) return;
 
-  const q = query(collection(db,"videos"), where("userId","==",user.uid));
-  const snap = await getDocs(q);
+if(!user){
+location.href="auth.html";
+return;
+}
 
-  snap.forEach(d=>{
-    const data = d.data();
+username.innerText = user.email;
 
-    const video = document.createElement("video");
-    video.src = data.video;
-    video.controls = true;
+const q = query(collection(db,"videos"), where("userId","==",user.uid));
 
-    profileVideos.appendChild(video);
-  });
+const snap = await getDocs(q);
+
+profileVideos.innerHTML="";
+
+snap.forEach(d=>{
+
+const v = document.createElement("video");
+
+v.src = d.data().video;
+
+v.controls = true;
+
+profileVideos.appendChild(v);
+
+});
+
 });
