5eee50f5821615d0f0dae8d8e7a680231f005ea5
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-10 23:07:49 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,55 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const currentUser="user1"
+const profileUser="user2"
+
+const followBtn=document.getElementById("followBtn")
+
+async function loadFollowStats(){
+
+const followersRef=collection(db,"users",profileUser,"followers")
+const followingRef=collection(db,"users",currentUser,"following")
+
+const followersSnap=await getDocs(followersRef)
+const followingSnap=await getDocs(followingRef)
+
+document.getElementById("followers").innerText=followersSnap.size
+document.getElementById("following").innerText=followingSnap.size
+
+}
+
+loadFollowStats()
+
+
+
+followBtn.onclick=async ()=>{
+
+const followDoc=doc(db,"users",profileUser,"followers",currentUser)
+
+const followingDoc=doc(db,"users",currentUser,"following",profileUser)
+
+const snap=await getDoc(followDoc)
+
+if(snap.exists()){
+
+await deleteDoc(followDoc)
+await deleteDoc(followingDoc)
+
+followBtn.innerText="Follow"
+
+}else{
+
+await setDoc(followDoc,{time:Date.now()})
+await setDoc(followingDoc,{time:Date.now()})
+
+followBtn.innerText="Following"
+
+}
+
+loadFollowStats()
+
+}
 const firebaseConfig = {
   apiKey: "AIzaSyDAl3TMOxgMKvw-hjqmhEyNTUj2_I25bgk",
   authDomain: "hridoy-er-apps-2ad7c.firebaseapp.com",
