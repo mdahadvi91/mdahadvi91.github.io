7b870fb581c82ebcf4fc790340ed8ff5610b089b
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-10 12:36:00 +0400

    Update feed.js

--- feed.js
+++ feed.js
@@ -5,55 +5,23 @@ const feed = document.getElementById("video-feed");
 
 async function loadVideos(){
 
-const snapshot = await getDocs(collection(db,"videos"));
+const snapshot = await getDocs(collection(db,"videos2"));
 
 snapshot.forEach((doc)=>{
 
 const data = doc.data();
-
-if(!data.video) return;
 
 const video = document.createElement("video");
 
 video.src = data.video;
 video.muted = true;
 video.loop = true;
+video.autoplay = true;
 video.playsInline = true;
-video.preload = "auto";
+video.controls = true;
 
 feed.appendChild(video);
 
-});
-
-autoPlay();
-}
-
-function autoPlay(){
-
-const videos = document.querySelectorAll("video");
-
-const observer = new IntersectionObserver((entries)=>{
-
-entries.forEach(entry=>{
-
-const video = entry.target;
-
-if(entry.isIntersecting){
-
-video.play();
-
-}else{
-
-video.pause();
-
-}
-
-});
-
-},{ threshold:0.7 });
-
-videos.forEach(video=>{
-observer.observe(video);
 });
 
 }
