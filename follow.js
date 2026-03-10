a738d6379d54566f2b6203962572e69079ca32a7
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-10 09:19:54 +0400

    Update follow.js

--- follow.js
+++ follow.js
@@ -1,14 +1,18 @@
 import { db } from "./firebase.js";
-import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { doc, updateDoc, increment } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const followBtn = document.getElementById("followBtn");
 
-followBtn.addEventListener("click", async ()=>{
+let profileId="defaultUser";
 
-await setDoc(doc(db,"follows","userFollow"),{
-follow:true
+followBtn.onclick = async ()=>{
+
+const ref = doc(db,"users",profileId);
+
+await updateDoc(ref,{
+followers:increment(1)
 });
 
 alert("Followed");
 
-});
+};
