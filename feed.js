03ca684d610e5fa43806c7b08287c80edccc6b7b
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-10 13:12:47 +0400

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,48 +1,5 @@
-import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const feed = document.getElementById("video-feed");
-
-async function loadVideos(){
-
-feed.innerHTML="Loading videos...";
-
-try{
-
-const snapshot = await getDocs(collection(db,"videos2"));
-
-console.log("Videos found:",snapshot.size);
-
-feed.innerHTML="";
-
-snapshot.forEach((doc)=>{
-
-const data = doc.data();
-
-console.log(data);
-
 const video = document.createElement("video");
-
-video.src = data.video;
-video.muted = true;
-video.loop = true;
-video.autoplay = true;
+video.src = "https://www.w3schools.com/html/mov_bbb.mp4";
 video.controls = true;
-
-video.style.width="100%";
-video.style.height="100vh";
-
+video.autoplay = true;
 feed.appendChild(video);
-
-});
-
-}catch(err){
-
-console.error(err);
-feed.innerHTML="Error loading videos";
-
-}
-
-}
-
-loadVideos();
