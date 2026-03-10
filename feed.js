655a4817c65c39edabe88bccbc448bf96a9422de
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-10 09:25:18 +0400

    Update feed.js

--- feed.js
+++ feed.js
@@ -3,11 +3,11 @@ import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/
 
 const feed = document.getElementById("video-feed");
 
-async function loadVideos() {
-
-feed.innerHTML = "";
+async function loadVideos(){
 
 const snapshot = await getDocs(collection(db,"videos"));
+
+feed.innerHTML="";
 
 snapshot.forEach((doc)=>{
 
@@ -16,53 +16,15 @@ const data = doc.data();
 const video = document.createElement("video");
 
 video.src = data.video;
+video.className="feed-video";
 
-video.className = "feed-video";
-
-video.autoplay = true;
-video.loop = true;
-video.muted = true;
-video.controls = false;
-video.playsInline = true;
+video.autoplay=true;
+video.loop=true;
+video.muted=true;
+video.playsInline=true;
 
 feed.appendChild(video);
 
-});
-
-autoPlay();
-
-}
-
-function autoPlay(){
-
-const videos = document.querySelectorAll(".feed-video");
-
-const observer = new IntersectionObserver((entries)=>{
-
-entries.forEach((entry)=>{
-
-const video = entry.target;
-
-if(entry.isIntersecting){
-
-videos.forEach(v=>{
-v.pause();
-});
-
-video.play();
-
-}else{
-
-video.pause();
-
-}
-
-});
-
-},{threshold:0.8});
-
-videos.forEach(v=>{
-observer.observe(v);
 });
 
 }
