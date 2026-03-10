5814d5d822e8c557f5d279dc8730246c2c1b8fba
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-10 09:34:48 +0400

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,27 +1,35 @@
 import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const feed = document.getElementById("video-feed");
 
-async function loadVideos(){
+/* LOAD VIDEOS */
 
-const snapshot = await getDocs(collection(db,"videos"));
+async function loadVideos(){
 
 feed.innerHTML="";
 
-snapshot.forEach((doc)=>{
+const querySnapshot = await getDocs(collection(db,"videos"));
+
+querySnapshot.forEach((doc)=>{
 
 const data = doc.data();
 
 const video = document.createElement("video");
 
-video.src = data.video;
-video.className="feed-video";
+video.src = data.url;
 
-video.autoplay=true;
-video.loop=true;
-video.muted=true;
-video.playsInline=true;
+video.controls = true;
+
+video.autoplay = true;
+
+video.loop = true;
+
+video.muted = false;
 
 feed.appendChild(video);
 
